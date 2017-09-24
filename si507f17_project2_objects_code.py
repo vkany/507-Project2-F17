c4be8616926ad5c94203a467acff154a71f860b4
Vibhuti Kanitkar <vibhuti@umich.edu>
2017-09-24 14:41:58 -0400

    first commit

--- si507f17_project2_objects_code.py
+++ si507f17_project2_objects_code.py
@@ -3,11 +3,12 @@
 import requests
 import json
 import unittest
+import pprint
 
 ## Instructions for each piece to be completed for this project can be found in the file, below.
 
 ## To see whether your problem solutions are passing the tests, you should run the Python file:
-# si507f17_project2_objects_tests.py, which should be saved in the same directory as this file. 
+# si507f17_project2_objects_tests.py, which should be saved in the same directory as this file.
 
 ## (DO NOT change the name of this file! Make sure to re-save it with the name si507f17_project2_objects_code.py if you change the name. Otherwise, we will not be able to grade it!)
 
@@ -27,8 +28,8 @@ print("\n*** *** PROJECT 2 *** ***\n")
 
 ## You can search for a variety of different types of media with the iTunes Search API: songs, movies, ebooks and audiobooks... (and more) You'll definitely need to check out the documentation to understand/recall how the parameters of this API work: https://affiliate.itunes.apple.com/resources/documentation/itunes-store-web-service-search-api/
 
-## Here, we've provided functions to get and cache data from the iTunes Search API, but looking at the information in that documentation will help you understand what is happening when the second function below gets invoked. 
-## Make sure you understand what the function does, how it works, and how you could invoke it to get data from iTunes Search about e.g. just songs corresponding to a certain search term, just movies, or just books. 
+## Here, we've provided functions to get and cache data from the iTunes Search API, but looking at the information in that documentation will help you understand what is happening when the second function below gets invoked.
+## Make sure you understand what the function does, how it works, and how you could invoke it to get data from iTunes Search about e.g. just songs corresponding to a certain search term, just movies, or just books.
 ## Refer to the textbook sections about caching, linked above, to help understand these functions!
 
 ## You may want to try them out and see what data gets returned, in order to complete the problems in this project.
@@ -65,13 +66,41 @@ def sample_get_cache_itunes_data(search_term,media_term="all"):
 		cache_file_ref.close()
 		return CACHE_DICTION[unique_ident]
 
+sample_data = sample_get_cache_itunes_data("Taylor Swift",media_term="all")
+# print(json.dumps(sample_data))
+test_diction = sample_data ['results'][0]
+
+# pprint.pprint(sample_data)
+
 
 ## [PROBLEM 1] [250 POINTS]
 print("\n***** PROBLEM 1 *****\n")
 
 
-## For problem 1, you should define a class Media, representing ANY piece of media you can find on iTunes search. 
+## For problem 1, you should define a class Media, representing ANY piece of media you can find on iTunes search.
+class Media(object):
 
+    def __init__(self, test_diction):
+        self.title  = test_diction['trackName']
+        self.author = test_diction[ 'artistName']
+        self.itunes_URL = test_diction['artistViewUrl']
+        self.itunes_id = test_diction['trackId']
+
+    def __str__(self):
+        return "{} by {}".format(self.title,self.author)
+
+    def __repr__(self):
+        return "ITUNES MEDIA: {}".format(self.itunes_id )
+
+    def __len__(self):
+        return 0
+
+    def __contains__(self, string):
+        return string in self.title
+
+#
+# x = Media(teat)
+# print(x)
 
 ## The Media class should accept one dictionary data structure representing a piece of media from iTunes as input to the constructor.
 ## Its constructor should invoke a method to get and cache data, and instatiate at least the following instance variables:
@@ -117,7 +146,7 @@ print("\n***** PROBLEM 2 *****\n")
 ## - rating (the content advisory rating, from the data)
 ## - genre
 ## - description (if none, the value of this instance variable should be None) -- NOTE that this might cause some string encoding problems for you to debug!
-## HINT: Check out the Unicode sub-section of the textbook! This is a common type of Python debugging you'll encounter with real data... but using the right small amount of code to fix it will solve all your problems. 
+## HINT: Check out the Unicode sub-section of the textbook! This is a common type of Python debugging you'll encounter with real data... but using the right small amount of code to fix it will solve all your problems.
 
 ## Should have the len method overridden to return the number of minutes in the movie (HINT: The data returns the number of milliseconds in the movie... how can you convert that to minutes?)
 
@@ -130,24 +159,35 @@ print("\n***** PROBLEM 3 *****\n")
 
 ## In this problem, you'll write some code to use the definitions you've just written.
 
-## First, here we have provided some variables which hold data about media overall, songs, movies, and books. 
+## First, here we have provided some variables which hold data about media overall, songs, movies, and books.
 
 ## NOTE: (The first time you run this file, data will be cached, so the data saved in each variable will be the same each time you run the file, as long as you do not delete your cached data.)
 
-media_samples = sample_get_cache_itunes_data("love")["results"]
+media_samples = sample_get_cache_itunes_data("love")["results"]#list of dictionaries with all media with the word love
 
-song_samples = sample_get_cache_itunes_data("love","music")["results"]
+song_samples = sample_get_cache_itunes_data("love","music")["results"] #list of dictionaries with all songs with the word love
 
-movie_samples = sample_get_cache_itunes_data("love","movie")["results"]
+movie_samples = sample_get_cache_itunes_data("love","movie")["results"] #list of dictionaries with all movies with the word love
 
+book_samples = sample_get_cache_itunes_data ("love","ebook")["results"]
 
 ## You may want to do some investigation on these variables to make sure you understand correctly what type of value they hold, what's in each one!
 
-## Use the values in these variables above, and the class definitions you've written, in order to create a list of each media type, including "media" generally. 
+## Use the values in these variables above, and the class definitions you've written, in order to create a list of each media type, including "media" generally.
+media_list = []
 
-## You should end up with: a list of Media objects saved in a variable media_list, 
-## a list of Song objects saved in a variable song_list, 
-## a list of Movie objects saved in a variable movie_list, 
+for media_item in media_samples:
+    instance = Media(media_item)
+    media_list.append(instance)
+
+print(media_list)
+
+#
+# print(book_list)
+
+## You should end up with: a list of Media objects saved in a variable media_list,
+## a list of Song objects saved in a variable song_list,
+## a list of Movie objects saved in a variable movie_list,
 ## and a list of Book objects saved in a variable book_list.
 
 ## You may use any method of accumulation to make that happen.
@@ -166,9 +206,9 @@ print("\n***** PROBLEM 4 *****\n")
 ## Each of those CSV files should have 5 columns each:
 # - title
 # - artist
-# - id 
-# - url (for the itunes url of that thing -- the url to view that track of media on iTunes) 
-# - length 
+# - id
+# - url (for the itunes url of that thing -- the url to view that track of media on iTunes)
+# - length
 
 ## There are no provided tests for this problem -- you should check your CSV files to see that they fit this description to see if this problem worked correctly for you. IT IS VERY IMPORTANT THAT YOUR CSV FILES HAVE EXACTLY THOSE NAMES!
 
@@ -176,25 +216,10 @@ print("\n***** PROBLEM 4 *****\n")
 
 ## HINT: You may want to think about what code could be generalized here, and what couldn't, and write a function or two -- that might make your programming life a little bit easier in the end, even though it will require more thinking at the beginning! But you do not have to do this.
 
-## HINT #2: *** You MAY add other, non-required, methods to the class definitions in order to make this easier, if you prefer to! 
+## HINT #2: *** You MAY add other, non-required, methods to the class definitions in order to make this easier, if you prefer to!
 
 ## It is perfectly fine to write this code in any way, as long as you rely on instances of the classes you've defined, and the code you write results in 3 correctly formatted CSV files!
 
 ## HINT #3: Check out the sections in the textbook on opening and writing files, and the section(s) on CSV files!
 
 ## HINT #4: Write or draw out your plan for this before you actually start writing the code! That will make it much easier.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
